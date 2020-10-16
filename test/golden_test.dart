ad6994ff685e52a79c1c34d4a6c33a584dc7ed5d
Nicolas Cuillery <ncuillery@Nicolass-MacBook-Pro.local>
2020-10-15 22:07:04 -0400

    Golden test

---
+++ test/golden_test.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_test_sandbox/main.dart';
+
+void main() {
+  setUpAll(() async {
+    // Load fonts
+    final fontFile = File('test/fonts/MaterialIcons-Regular.otf');
+    final fontData = await fontFile.readAsBytes();
+    final fontLoader = FontLoader('MaterialIcons');
+    fontLoader.addFont(Future.value(ByteData.view(fontData.buffer)));
+    await fontLoader.load();
+
+    final fontFile2 = File('test/fonts/Roboto-Regular.ttf');
+    final fontData2 = await fontFile2.readAsBytes();
+    final fontLoader2 = FontLoader('Roboto');
+    fontLoader2.addFont(Future.value(ByteData.view(fontData2.buffer)));
+    await fontLoader2.load();
+  });
+
+  testWidgets('App is rendered', (tester) async {
+    await tester.pumpWidget(MyApp());
+
+    await expectLater(
+      find.byType(Scaffold),
+      matchesGoldenFile('home_page.png'),
+    );
+  });
+}
